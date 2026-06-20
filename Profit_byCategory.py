cf32e1dca0f7618644d67f1829d115f9d342831f
Likhitha1026 <likhithakamireddy88@gmail.com>
2026-06-20 22:29:40 +0530

    Delete Profit_byCategory.py

--- Profit_byCategory.py
+++
@@ -1,9 +0,0 @@
-import pandas as py
-import matplotlib.pyplot as plt
-df=py.read_excel("samplesuperstore_cleaned.xlsx")
-profit_by_category=df.groupby("Category")["Profit"].sum()
-profit_by_category.plot(kind="bar")
-plt.title("Profit by Category")
-plt.xlabel("Category")
-plt.ylabel("Total Profit")
-plt.show()
