17619a73f14f0d2e5b5dcf0bf8eb0b35b0502556
Likhitha1026 <likhithakamireddy88@gmail.com>
2026-06-26 12:12:31 +0530

    Delete Sales_byCategory.py

--- Sales_byCategory.py
+++
@@ -1,9 +0,0 @@
-import pandas as py
-import matplotlib.pyplot as plt
-df=py.read_excel("samplesuperstore_cleaned.xlsx")
-sales_by_category=df.groupby("Category")["Sales"].sum()
-sales_by_category.plot(kind="bar")
-plt.title("Sales by Category")
-plt.xlabel("Category")
-plt.ylabel("Total Sales")
-plt.show()
