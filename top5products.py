ca8890ed3bcaa7c697db55a8c4a11f50da3702b1
Likhitha1026 <likhithakamireddy88@gmail.com>
2026-06-26 12:15:20 +0530

    Delete top5products.py

--- top5products.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_excel("samplesuperstore_cleaned.xlsx")
-top_products = df.groupby("Product Name")["Sales"].sum()
-top_products = top_products.sort_values(ascending=False)
-print(top_products.head(5))
