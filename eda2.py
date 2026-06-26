1ea23f9431ddd6b54d5e88453cafa6482cf5a5b0
Likhitha1026 <likhithakamireddy88@gmail.com>
2026-06-26 12:13:29 +0530

    Delete eda2.py

--- eda2.py
+++
@@ -1,7 +0,0 @@
-import pandas as py
-import matplotlib.pyplot as plt
-df=py.read_excel("samplesuperstore_cleaned.xlsx")
-sales_by_category=df.groupby("Category")["Sales"].sum()
-print(sales_by_category)
-profit_by_category=df.groupby("Category")["Profit"].sum()
-print(profit_by_category)
