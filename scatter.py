67b02d1e8cbc605af35b250334a3c5c18a5ab5c6
Likhitha1026 <likhithakamireddy88@gmail.com>
2026-06-26 12:14:33 +0530

    Delete scatter.py

--- scatter.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.read_excel("samplesuperstore_cleaned.xlsx")
-plt.figure(figsize=(8,5))
-plt.scatter(df["Sales"], df["Profit"])
-plt.title("Sales vs Profit")
-plt.xlabel("Sales")
-plt.ylabel("Profit")
-plt.show()
