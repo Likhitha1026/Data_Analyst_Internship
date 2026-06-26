8ede4eedb76d3c4c2c1d635ac086d89cd0e49a70
Likhitha1026 <likhithakamireddy88@gmail.com>
2026-06-26 12:13:50 +0530

    Delete heatmap.py

--- heatmap.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-df = pd.read_excel("samplesuperstore_cleaned.xlsx")
-corr = df[["Sales", "Profit", "Quantity", "Discount"]].corr()
-sns.heatmap(corr, annot=True)
-plt.title("Correlation Heatmap")
-plt.show()
